4497f905b4f76abca7c1ac19cf7e05b0b5767178
Arif-khan9 <ak4040651@gmail.com>
2026-03-10 12:25:08 +0530

    fix typescript splittext animation

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -4,7 +4,7 @@ import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
 import { SplitText } from "gsap-trial/SplitText";
 
 interface ParaElement extends HTMLElement {
-  anim?: gsap.core.Animation;
+  anim?: gsap.core.Tween;
   split?: SplitText;
 }
 
@@ -12,7 +12,9 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
+
   if (window.innerWidth < 900) return;
+
   const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
   const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
@@ -21,6 +23,7 @@ export default function setSplitText() {
 
   paras.forEach((para: ParaElement) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -31,50 +34,57 @@ export default function setSplitText() {
       linesClass: "split-line",
     });
 
+    if (!para.split) return;
+
     para.anim = gsap.fromTo(
       para.split.words,
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+        stagger: 0.02,
         scrollTrigger: {
           trigger: para.parentElement?.parentElement,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
-        duration: 1,
-        ease: "power3.out",
-        y: 0,
-        stagger: 0.02,
       }
     );
   });
+
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
+    if (!title.split) return;
+
     title.anim = gsap.fromTo(
       title.split.chars,
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
+        y: 0,
+        rotate: 0,
+        duration: 0.8,
+        ease: "power2.inOut",
+        stagger: 0.03,
         scrollTrigger: {
           trigger: title.parentElement?.parentElement,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
-        duration: 0.8,
-        ease: "power2.inOut",
-        y: 0,
-        rotate: 0,
-        stagger: 0.03,
       }
     );
   });
 
-  ScrollTrigger.addEventListener("refresh", () => setSplitText());
+  ScrollTrigger.addEventListener("refresh", setSplitText);
 }
